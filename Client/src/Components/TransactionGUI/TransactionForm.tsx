87c7b5a489d761294c1d63cbfb1bf778ae8de704
rjhakes <rjhakes@yahoo.com>
2022-01-04 01:26:31 -0700

    initting transGui

---
+++ Client/src/Components/TransactionGUI/TransactionForm.tsx
@@ -0,0 +1,11 @@
+import { observer } from 'mobx-react-lite';
+import React, { useState } from 'react';
+
+export default observer( function TransactionForm() {
+
+    return (
+        <>
+            
+        </>
+    )
+})
