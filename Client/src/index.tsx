359a9266c6448fd457449ea20aab4aca6644cb19
Richard Hakes <rjhakes@yahoo.com>
2023-04-17 10:30:06 -0400

    migrating client

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
-import './app/layout/styles.css';
-import App from './app/layout/App';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store, StoreContext } from './app/stores/store';
 
-ReactDOM.render(
-  <StoreContext.Provider value={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  </StoreContext.Provider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
